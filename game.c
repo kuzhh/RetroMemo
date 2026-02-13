dc2226da0982b0a061c08bc37f9657788034ec33
AntonellaGahonaR <130939529+AntonellaGahonaR@users.noreply.github.com>
2026-02-12 23:16:47 -0300

    Update game.c

--- game.c
+++ game.c
@@ -1,8 +1,23 @@
 #include "game.h"
+/*
 #include "input.h"
 #include "assets.h"
 #include "screen.h"      
 #include <string.h>     
 #include <stdlib.h>      
-#include <time.h> 
+#include <time.h> // biblios que podriamos agregar, por las dudas lo dejo asi */
 
+
+/*FUNCION PARA MEZCLAR CARTAS (capaz sirve) 
+static void mezclar(int* a, int n)
+{
+    for(int i = 0; i < n - 1; i++)// recorro
+    {
+        int j = i + rand() % (n - i);// elijo una pos al azar
+
+        int aux = a[i];// intercambio
+        a[i] = a[j];
+        a[j] = aux;
+    }
+}
+*/
