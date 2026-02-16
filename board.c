9871acc402d146db57073665d6a09a6ffcb6b37b
Rodrigo9702 <castillorod.n@gmail.com>
2026-02-16 15:21:59 -0300

    FIX 4x5 ajustado W H de las cards

--- board.c
+++ board.c
@@ -3,149 +3,145 @@
 #define SOUND_MATCHED "snd/Correcto.wav"
 #define SOUND_NOT_MATCHED "snd/Fallo.wav"
 
-void shuffleCards(tBoard* board)
-{
-    srand((unsigned int)time(NULL));
+void shuffleCards(tBoard *board) {
+  srand((unsigned int)time(NULL));
 
-    for(int i = 0; i < board->totalCards; i++)
-    {
-        int j = rand() % board->totalCards;
+  for (int i = 0; i < board->totalCards; i++) {
+    int j = rand() % board->totalCards;
 
-        tCard temp = board->cards[i];
-        board->cards[i] = board->cards[j];
-        board->cards[j] = temp;
-    }
+    tCard temp = board->cards[i];
+    board->cards[i] = board->cards[j];
+    board->cards[j] = temp;
+  }
 }
 
-int boardInit(tBoard* board, int rows, int cols)
-{
-    if(!board || rows <= 0 || cols <= 0)
-        return ERR;
+int boardInit(tBoard *board, int rows, int cols) {
+  if (!board || rows <= 0 || cols <= 0)
+    return ERR;
 
-    board->rows = rows;
-    board->cols = cols;
-    board->totalCards = (rows * cols);
+  board->rows = rows;
+  board->cols = cols;
+  board->totalCards = (rows * cols);
 
-    if((board->totalCards == 0) || (board->totalCards % 2 != 0))
-    {
-        printf("Error board: we need pairs for playing.\n");
-        return ERR;
-    }
+  if ((board->totalCards == 0) || (board->totalCards % 2 != 0)) {
+    printf("Error board: we need pairs for playing.\n");
+    return ERR;
+  }
 
-    board->cards = malloc(sizeof(tCard) * board->totalCards);
-    if(!board->cards)
-    {
-        printf("Error creating board: no memory available");
-        return ERR;
-    }
+  board->cards = malloc(sizeof(tCard) * board->totalCards);
+  if (!board->cards) {
+    printf("Error creating board: no memory available");
+    return ERR;
+  }
 
-    for(int i=0; i<board->totalCards; i++)
-    {
-        board->cards[i].isFlipped = 0;
-        board->cards[i].isMatched = 0;
-        board->cards[i].id = i / 2;
+  for (int i = 0; i < board->totalCards; i++) {
+    board->cards[i].isFlipped = 0;
+    board->cards[i].isMatched = 0;
+    board->cards[i].id = i / 2;
 
-        //cargo sonido cartas
-        board->cards[i].sound_Matched = sound_load(SOUND_MATCHED);
-        board->cards[i].sound_Not_Matched = sound_load(SOUND_NOT_MATCHED);
-    }
+    // cargo sonido cartas
+    board->cards[i].sound_Matched = sound_load(SOUND_MATCHED);
+    board->cards[i].sound_Not_Matched = sound_load(SOUND_NOT_MATCHED);
+  }
 
-    shuffleCards(board);
+  shuffleCards(board);
 
-    return OK;
+  return OK;
 }
 
-void boardDestroy(tBoard* board)
-{
-    if(!board->cards)
-        return;
+void boardDestroy(tBoard *board) {
+  if (!board->cards)
+    return;
 
-    for(int i=0;i<board->totalCards;i++){
-        sound_destroy(board->cards[i].sound_Matched);
-        sound_destroy(board->cards[i].sound_Not_Matched);
-    }
-    
-    free(board->cards);
-    board->cards = NULL;
+  for (int i = 0; i < board->totalCards; i++) {
+    sound_destroy(board->cards[i].sound_Matched);
+    sound_destroy(board->cards[i].sound_Not_Matched);
+  }
+
+  free(board->cards);
+  board->cards = NULL;
 }
 
-void boardRender(SDL_Renderer* renderer, tBoard* board, tCardSet* card)
-{
-    if(!board->cards || !card)
-        return;
+void boardRender(SDL_Renderer *renderer, tBoard *board, tCardSet *card) {
+  if (!board->cards || !card)
+    return;
 
-    int hSpacing = 30;
-    int wSpacing = 25;
+  int hSpacing = 30;
+  int wSpacing = 25;
+  int cardW = CARD_W;
+  int cardH = CARD_H;
 
-    if(board->cols == 5)
-        hSpacing = 15;
+  if (board->cols == 5) {
+    hSpacing = 15;
+    cardW = 200;
+    cardH = 112;
+  }
 
-    int totalW = board->cols * CARD_W + (board->cols -1) * hSpacing;
-    int totalH = board->rows * CARD_H + (board->rows -1) * wSpacing;
+  int totalW = board->cols * cardW + (board->cols - 1) * hSpacing;
+  int totalH = board->rows * cardH + (board->rows - 1) * wSpacing;
 
-    int startX = (SCREEN_WIDTH -totalW) /2;
-    int startY = (SCREEN_HEIGHT -totalH) /2;
+  int startX = (SCREEN_WIDTH - totalW) / 2;
+  int startY = (SCREEN_HEIGHT - totalH) / 2;
 
-    for(int i=0; i < board->totalCards; i++)
-    {
-        int row = i / board->cols;
-        int col = i % board->cols;
+  for (int i = 0; i < board->totalCards; i++) {
+    int row = i / board->cols;
+    int col = i % board->cols;
 
-        SDL_Rect dest = {
-            .x = startX + col * (CARD_W + hSpacing),
-            .y = startY + row * (CARD_H + wSpacing),
-            .w = CARD_W,
-            .h = CARD_H
-        };
+    SDL_Rect dest = {.x = startX + col * (cardW + hSpacing),
+                     .y = startY + row * (cardH + wSpacing),
+                     .w = cardW,
+                     .h = cardH};
 
-        if(board->cards[i].isFlipped || board->cards[i].isMatched)
-        {
-            SDL_RenderCopy(renderer, card->cardFront[board->cards[i].id], NULL, &dest);
-        }
-        else
-        {
-            SDL_RenderCopy(renderer, card->cardBack, NULL, &dest);
-        }
+    if (board->cards[i].isFlipped || board->cards[i].isMatched) {
+      SDL_RenderCopy(renderer, card->cardFront[board->cards[i].id], NULL,
+                     &dest);
+    } else {
+      SDL_RenderCopy(renderer, card->cardBack, NULL, &dest);
     }
+  }
 }
 
-int boardGetCardAt(tBoard* board, int mouseX, int mouseY)
-{
-    if(!board || !board->cards)//evita errores
-    return -1; //evita errores
+int boardGetCardAt(tBoard *board, int mouseX, int mouseY) {
+  if (!board || !board->cards) // evita errores
+    return -1;                 // evita errores
 
-    int hSpacing = 30;
-    int wSpacing = 25;
+  int hSpacing = 30;
+  int wSpacing = 25;
+  int cardW = CARD_W;
+  int cardH = CARD_H;
 
-    if(board->cols == 5)
-        hSpacing = 15;
+  if (board->cols == 5) {
+    hSpacing = 15;
+    cardW = 200;
+    cardH = 112;
+  }
 
-    int totalW = board->cols * CARD_W + (board->cols -1) * hSpacing;
-    int totalH = board->rows * CARD_H + (board->rows -1) * wSpacing;
+  int totalW = board->cols * cardW + (board->cols - 1) * hSpacing;
+  int totalH = board->rows * cardH + (board->rows - 1) * wSpacing;
 
-    int startX = (SCREEN_WIDTH -totalW) /2;
-    int startY = (SCREEN_HEIGHT -totalH) /2;
+  int startX = (SCREEN_WIDTH - totalW) / 2;
+  int startY = (SCREEN_HEIGHT - totalH) / 2;
 
-    if(mouseX < startX || mouseY < startY)
-        return -1;
+  if (mouseX < startX || mouseY < startY)
+    return -1;
 
-    if(mouseX > startX + totalW || mouseY > startY + totalH)
-        return -1;
+  if (mouseX > startX + totalW || mouseY > startY + totalH)
+    return -1;
 
-    int col = (mouseX - startX) / (CARD_W + hSpacing);
-    int row = (mouseY - startY) / (CARD_H + wSpacing);
+  int col = (mouseX - startX) / (cardW + hSpacing);
+  int row = (mouseY - startY) / (cardH + wSpacing);
 
-    if(col < 0 || col >= board->cols || row < 0 || row >= board->rows)
-        return -1;
+  if (col < 0 || col >= board->cols || row < 0 || row >= board->rows)
+    return -1;
 
-    int cardX = startX + col * (CARD_W + hSpacing);
-    int cardY = startY + row * (CARD_H + wSpacing);
+  int cardX = startX + col * (cardW + hSpacing);
+  int cardY = startY + row * (cardH + wSpacing);
 
-    if(mouseX > cardX + CARD_W)
-        return -1;
+  if (mouseX > cardX + cardW)
+    return -1;
 
-    if(mouseY > cardY + CARD_H)
-        return -1;
+  if (mouseY > cardY + cardH)
+    return -1;
 
-    return row * board->cols +col;
+  return row * board->cols + col;
 }
